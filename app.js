991b9aca38e66af628cc77830aff7507b0a78152
sclarke500 <sclarke500@gmail.com>
2023-02-10 09:53:32 -0500

    test4

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ Office.onReady((info) => {
   Office.addin.showAsTaskpane();
   console.log('tried to show taskpane');
 
-  Office.context.document.settings.set("Office.AutoShowTaskpaneWithDocument", true);
-  Office.context.document.settings.saveAsync();
+  Office.addin.setStartupBehavior(Office.StartupBehavior.load);
 
 })
 
